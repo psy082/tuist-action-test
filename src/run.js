2edc233518085f0e9d1228c86f01a60773da53a1
psy082 <zhappy114@gmail.com>
2022-04-16 03:33:26 +0900

    modify execution command

--- src/run.js
+++ src/run.js
@@ -14,7 +14,7 @@ module.exports = async () => {
     ensureDarwin();
     await installTuist();
 
-    let execCommand = `${tuistEnvPath} ${command}`;
+    let execCommand = `${tuistEnvPath}/tuistexec ${command}`;
     if (args) {
       execCommand = `${execCommand} ${args}`;
     }
